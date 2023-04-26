da9dff55fa27cf3aebc41c318833f5591538e1fb
askuy <askuygo@foxmail.com>
2023-04-26 16:11:38 +0800

    增加DbName解析

--- component.go
+++ component.go
@@ -9,6 +9,7 @@ const PackageName = "component.emongo"
 // Component client (cmdable and config)
 type Component struct {
 	config *config
+	dbName string // dbname 解析后的dbName放到这里
 	client *Client
 	logger *elog.Component
 }
@@ -16,4 +17,9 @@ type Component struct {
 // Client returns emongo Client
 func (c *Component) Client() *Client {
 	return c.client
+}
+
+// DbName returns emongo Client
+func (c *Component) DbName() string {
+	return c.dbName
 }
